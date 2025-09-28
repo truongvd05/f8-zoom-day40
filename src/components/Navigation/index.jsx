c57f9f456c5401502c3bcd432fb78bd74549cd8e
truongvd05 <truongvdf8@fullstack.edu.vn>
2025-09-28 19:08:20 +0700

    fix navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -7,16 +7,13 @@ function Navigation() {
             link: "/",
             title: "Home",
         },
-        {
-            link: "http://localhost:5173/f8-zoom-day40/redux.html",
-            title: "redux core",
-        },
     ]
     return (
         <ul className={style.container}>
             {nav.map((item, index) => (
                 <li key={index}>
-                    <a href={item.link}>{item.title}</a>
+                    <NavLink to={item.link}>{item.title}</NavLink>
+                    <a target="_blank" href="redux.html">Redux Demo</a>
                 </li>
             ))}
         </ul>
